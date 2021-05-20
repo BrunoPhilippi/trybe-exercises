6e563b5706293d592bd1fceebef06e8e49b7d685
Bruno <brunophilippitrybe@gmail.com>
2021-05-20 18:28:48 -0300

    Exercicio 3

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2/exercicio2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2/exercicio2.js
@@ -2,9 +2,9 @@ let number1 = 87;
 let number2 = 90 ;
 
 if ( number1 > number2 ){
-    console.log("Numero 1 maior que Numero 2");
+    console.log(number1);
 
 } 
 else {
-    console.log("Numero 2 maior que numero 1");
+    console.log(number2);
 }
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2/exercicio3.js/exercicio3.js
@@ -0,0 +1,13 @@
+let number1 = 81;
+let number2 = 67;
+let number3 = 12;
+
+if (number1 > number3 && number2 < number1) {
+    console.log(number1);
+}
+else if (number2 > number1 && number1 < number2) {
+    console.log(number2);
+}
+else {
+    console.log(number3);
+}
