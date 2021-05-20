e4dd138ee2cdaf1249d7b606eb05da847e8320d9
Bruno <brunophilippitrybe@gmail.com>
2021-05-20 17:46:29 -0300

    exercicio2 retorno qual numero e maior

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2/exercicio2.js
@@ -0,0 +1,10 @@
+let number1 = 87;
+let number2 = 90 ;
+
+if ( number1 > number2 ){
+    console.log("Numero 1 maior que Numero 2");
+
+} 
+else {
+    console.log("Numero 2 maior que numero 1");
+}
